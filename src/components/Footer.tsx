509b5d14b65a55578e39df83d56dc8c0fc80d410
Nii Tettey <michaelperryt97@gmail.com>
2024-11-10 22:32:15 +0000

    Added route to source code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from "react";
 import { Icons } from "@/assets";
 import Image from "next/image";
+import Link from "next/link";
+import { FiGithub } from "react-icons/fi";
 import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
@@ -54,12 +56,26 @@ const Footer = () => {
             <span className="text-green-600 bg-slate-600">{currentYear}</span>
             .11.10/16{" "}
           </div>
+
           <div className="flex flex-row items-center justify-center gap-1">
-            <div className={`h-2 w-2 rounded-full bg-${statusColor}`}></div>
+            <div className={`h-2 w-2 rounded-full bg-green-600`}></div>
             <h3 className="text-slate-200">{systemStatusMessage}</h3>
           </div>
+          <Link
+            href="https://github.com/michaelperryjnr/themefolio"
+            target="blank"
+            className="flex flex-row items-center justify-center gap-3 border-[1.6px] border-solid bg-[#424242] border-[#afafaf] border-opacity-20 rounded-xl py-1 px-2 bg-opacity-40 sm:rounded-2xl"
+          >
+            <FiGithub size={15} className="text-white" />
+            Source Code
+          </Link>
         </div>
-        <div>
+        <div className="flex flex-col gap-[0.1rem]">
+          <Image
+            src={Icons.outlinedLogo}
+            alt="Logo"
+            className="w-10 h-10 sm:w-20 sm:h-20"
+          />
           <Image
             src={Icons.outlinedLogo}
             alt="Logo"
