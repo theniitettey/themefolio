2f8d3f343d48d73dc8ff3944b9f387034d5872c6
Nii Tettey <michaelperryt97@gmail.com>
2024-11-10 14:41:04 +0000

    Default asset export

---
+++ src/assets/index.ts
@@ -0,0 +1,14 @@
+import coloredLogo from "./icons/colored-logo.svg";
+import doodle from "./icons/doodle.svg";
+import projectImage from "./images/project-image.png";
+
+const Icons = {
+  coloredLogo,
+  doodle,
+};
+
+const Images = {
+  projectImage,
+};
+
+export { Icons, Images };
