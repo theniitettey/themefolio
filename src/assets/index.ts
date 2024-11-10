905934ebac5ed8ca0b3f7894b15486ca1b51bec1
Nii Tettey <michaelperryt97@gmail.com>
2024-11-10 19:17:09 +0000

    Default export

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,14 @@
 import coloredLogo from "./icons/colored-logo.svg";
+import outlinedLogo from "./icons/outlined-logo.svg";
 import doodle from "./icons/doodle.svg";
 import projectImage from "./images/project-image.png";
+import homeBg from "./icons/home-bg.svg";
 
 const Icons = {
   coloredLogo,
   doodle,
+  outlinedLogo,
+  homeBg,
 };
 
 const Images = {
