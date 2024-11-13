a3e39c9219486acd93f1558bf2a36a23c64d7042
Nii Tettey <michaelperryt97@gmail.com>
2024-11-13 18:41:52 +0000

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,85 +1,94 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Icons } from "@/assets";
-import Image from "next/image";
 import Link from "next/link";
 import { FiGithub } from "react-icons/fi";
-import { Roboto } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 
-const roboto = Roboto({
+const robotoMono = Roboto_Mono({
   weight: ["400", "500", "700"],
-  display: "swap",
+  style: ["normal", "italic"],
   subsets: ["latin"],
-  adjustFontFallback: true,
+  display: "swap",
 });
 
 const Footer = () => {
-  const [currentYear, setCurrentYear] = useState<number>(
-    new Date().getFullYear()
-  );
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
+  const [currentDay, setCurrentDay] = useState(new Date().getDate());
 
   const systemStatus = [
-    "All systems are operational",
-    "System is under maintenance",
-    "System is down",
-    "Checking system status...",
-  ];
+    "Operational",
+    "Down",
+    "Degraded",
+    "Checking...",
+    "Maintenance",
+  ] as const; // 'as const' makes this a readonly tuple of literal types
 
-  const [systemStatusMessage, setSystemStatusMessage] = useState<string>(
-    systemStatus[3]
+  type SystemStatus = (typeof systemStatus)[number]; // Union type of the possible statuses
+
+  const statusColors: Record<SystemStatus, string> = {
+    Operational: "bg-green-500",
+    Down: "bg-red-500",
+    Degraded: "bg-yellow-500",
+    "Checking...": "bg-cyan-600",
+    Maintenance: "bg-yellow-500",
+  };
+
+  const [systemStatusMessage, setSystemStatusMessage] =
+    useState<SystemStatus>("Checking...");
+  const [systemStatusColor, setSystemStatusColor] = useState(
+    statusColors["Checking..."]
   );
 
   useEffect(() => {
-    setTimeout(() => {
-      setSystemStatusMessage(systemStatus[0]);
-    }, 5000);
-  });
+    const randomizeInterval = setInterval(() => {
+      const randomStatus = systemStatus[
+        Math.floor(Math.random() * (systemStatus.length - 1))
+      ] as SystemStatus;
+      setSystemStatusColor(statusColors[randomStatus]);
+    }, 500);
+
+    const timeout = setTimeout(() => {
+      clearInterval(randomizeInterval);
+      const finalStatusIndex = Math.floor(
+        Math.random() * (systemStatus.length - 1)
+      );
+      const finalStatus = systemStatus[finalStatusIndex] as SystemStatus;
+      setSystemStatusMessage(finalStatus);
+      setSystemStatusColor(statusColors[finalStatus]);
+    }, 2000);
+
+    return () => {
+      clearInterval(randomizeInterval);
+      clearTimeout(timeout);
+    };
+  }, []);
 
   useEffect(() => {
     setCurrentYear(new Date().getFullYear());
   }, []);
-  return (
-    <footer className="w-full mt-4 sm:mt-8 pb-2">
-      <div className="h-[1px] w-full bg-ground-100 dark:bg-slate-400 mb-4 sm:mb-8"></div>
-      <div className="flex flex-row justify-between items-center">
-        <div className="flex flex-col items-start justify-start gap-1 sm:gap-3 text-xs sm:text-xl font-medium dark:text-slate-400 text-grey-100">
-          <div>
-            &copy;{" " + currentYear} Michael Perry Tettey. All rights reserved.{" "}
-          </div>
-          <div className={`${roboto.className}`}>
-            version:{" "}
-            <span className="text-green-600 dark:bg-slate-600">
-              {currentYear}
-            </span>
-            .11.10/16{" "}
-          </div>
 
-          <div className="flex flex-row items-center justify-center gap-1">
-            <div className={`h-2 w-2 rounded-full bg-green-600`}></div>
-            <h3 className="dark:text-slate-200 text-grey-200">
-              {systemStatusMessage}
-            </h3>
+  return (
+    <footer className="bg-background text-grey py-4 fixed bottom-0 left-0 right-0 z-50 text-xs sm:text-lg mt-2 border-t-[1.4px] border-solid border-t-slate-800">
+      <div className="px-4 flex justify-between items-center">
+        <Link href="/" className="dark:text-glow-100 text-glow-200">
+          © Nii Tettey
+        </Link>
+        <div className="flex flex-row items-center justify-center gap-2">
+          <div className={`${systemStatusColor} w-2 h-2 rounded-full`}></div>
+          <div className={`text-grey-500 ${robotoMono.className}`}>
+            {systemStatusMessage}
           </div>
-          <Link
-            href="https://github.com/michaelperryjnr/themefolio"
-            target="blank"
-            className="dark:text-ground-300 border-[1.3px] dark:bg-ground-300 dark:border-ground-300 bg-glow-200 border-glow-200 text-grey-100 border-opacity-25 dark:bg-opacity-25 dark:border-opacity-25 border-solid bg-opacity-25 py-1 px-2 rounded-xl text-xs sm:text-lg font-medium flex flex-row gap-1 w-fit items-center justify-center"
-          >
-            <FiGithub size={15} />
-            Source Code
-          </Link>
         </div>
-        <Link href="/" className="flex flex-col gap-[0.1rem]">
-          <Image
-            src={Icons.outlinedLogo}
-            alt="Logo"
-            className="w-10 h-10 sm:w-20 sm:h-20"
-          />
-          <Image
-            src={Icons.outlinedLogo}
-            alt="Logo"
-            className="w-10 h-10 sm:w-20 sm:h-20"
-          />
+        <div className="text-grey-500">
+          {currentYear}.{currentMonth}.{currentDay}
+        </div>
+        <Link
+          href="https://github.com/your-github-username/your-repo"
+          target="_blank"
+          className="border-solid dark:bg-ground-400 dark:border-ground-500 border-ground-700 bg-ground-700 border-opacity-20 rounded-xl p-2 dark:bg-opacity-40 bg-opacity-20 sm:p-3 sm:rounded-2xl"
+        >
+          <FiGithub size={15} />
         </Link>
       </div>
     </footer>
