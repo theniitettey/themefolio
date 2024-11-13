b665853f7f8169bf5fd99706bd8d90315eeb4cd8
Nii Tettey <michaelperryt97@gmail.com>
2024-11-13 18:39:24 +0000

    Created archive page

---
+++ src/app/archive/page.tsx
@@ -0,0 +1,44 @@
+import { allPosts } from "@/.contentlayer/generated";
+import Link from "next/link";
+import React from "react";
+
+export default function ArchivePage() {
+  const archivedPosts = allPosts.filter((post) => post.archived);
+
+  return (
+    <div>
+      <h1 className="text-xl sm:text-3xl font-bold flex gap-2 items-center mt-6 mb-4 cursor-pointer">
+        Archived Posts
+      </h1>
+      {archivedPosts.length > 0 ? (
+        archivedPosts.map((post) => (
+          <article key={post._id} className="mb-8">
+            <Link href={post.slug} className="flex justify-between items-start">
+              <h2
+                className="
+
+                text-sm
+                sm:text-lg
+                text-grey-100
+                dark:text-white
+                font-medium
+              "
+              >
+                {post.title}
+              </h2>
+            </Link>
+            {post.description && (
+              <p className="text-[0.7rem] sm:text-sm font-light text-ground-600">
+                {post.description}
+              </p>
+            )}
+          </article>
+        ))
+      ) : (
+        <p className="text-[0.7rem] sm:text-sm font-light text-ground-600">
+          No archived posts found.
+        </p>
+      )}
+    </div>
+  );
+}
