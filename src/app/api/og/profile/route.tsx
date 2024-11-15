379f50b237f22043be17175d786b86d3dfce8f98
Nii Tettey <michaelperryt97@gmail.com>
2024-11-15 02:20:09 +0000

    Added poppins locally

--- src/app/api/og/profile/route.tsx
+++ src/app/api/og/profile/route.tsx
@@ -1,22 +1,14 @@
 import { ImageResponse } from "next/dist/compiled/@vercel/og";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-async function loadGoogleFont(font: string, text: string) {
-  const url =
-    "https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap";
-  const css = await (await fetch(url)).text();
-  const resource = css.match(
-    /src: url\((.+)\) format\('(opentype|truetype)'\)/
-  );
-
-  if (resource) {
-    const response = await fetch(resource[1]);
-    if (response.status == 200) {
-      return await response.arrayBuffer();
-    }
-  }
-
-  throw new Error("failed to load font data");
-}
+const font = fs.promises.readFile(
+  path.join(
+    fileURLToPath(import.meta.url),
+    "../../../../fonts/Poppins-Regular.ttf"
+  )
+);
 
 export async function GET() {
   return new ImageResponse(
@@ -27,7 +19,7 @@ export async function GET() {
             "linear-gradient(to right, #24243e, #302b63, #0f0c29)",
           fontFamily: "Poppins, sans-serif",
         }}
-        tw="flex flex-row w-[1200px] h-[630px] items-center justify-between px-24 py-16  border-8 border-solid border-slate-800 shadow-2xl shadow-black/30 rounded-lg"
+        tw="flex flex-row w-[1200px] h-[630px] items-center justify-between px-24 py-16 border-8 border-solid border-slate-800 shadow-2xl shadow-black/30 rounded-lg"
       >
         <div tw="flex flex-col w-[70%] space-y-4">
           <h2 tw="text-6xl font-extrabold tracking-tight text-white leading-tight mb-4">
@@ -58,7 +50,7 @@ export async function GET() {
       fonts: [
         {
           name: "Poppins",
-          data: await loadGoogleFont("Poppins", "Michael Perry Nii Tettey"),
+          data: await font,
           style: "normal",
         },
       ],
--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,22 +1,14 @@
 import { ImageResponse } from "next/dist/compiled/@vercel/og";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-async function loadGoogleFont(font: string, text: string) {
-  const url =
-    "https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap";
-  const css = await (await fetch(url)).text();
-  const resource = css.match(
-    /src: url\((.+)\) format\('(opentype|truetype)'\)/
-  );
-
-  if (resource) {
-    const response = await fetch(resource[1]);
-    if (response.status == 200) {
-      return await response.arrayBuffer();
-    }
-  }
-
-  throw new Error("failed to load font data");
-}
+const font = fs.promises.readFile(
+  path.join(
+    fileURLToPath(import.meta.url),
+    "../../../fonts/Poppins-Regular.ttf"
+  )
+);
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -62,7 +54,7 @@ export async function GET(request: Request) {
       fonts: [
         {
           name: "Poppins",
-          data: await loadGoogleFont("Poppins", title),
+          data: await font,
           style: "normal",
         },
       ],
