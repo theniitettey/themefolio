72b7649a0a64a7572c701d8d00672a43db24e249
Nii Tettey <michaelperryt97@gmail.com>
2024-11-15 21:31:06 +0000

    Update api runtime

--- src/app/api/status/route.tsx
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-export const runtime = "nodejs";
-
-export function GET(request: NextRequest) {
-  return NextResponse.json({ status: "online" });
-}
