a52974951cba7c30248110c2efbed67ff00aa30b
Michael Perry Tettey <michaelperryt97@gmail.com>
2024-12-11 15:39:35 +0000

    Update Motion.tsx

--- src/components/ui/Motion.tsx
+++ src/components/ui/Motion.tsx
@@ -1,9 +1,9 @@
 "use client";
-
 import { motion } from "framer-motion";
+import { HTMLMotionProps } from "framer-motion";
 
-const MotionDiv = motion.div;
-const MotionHeader = motion.header;
-const MotionFooter = motion.footer;
+const MotionDiv: React.FC<HTMLMotionProps<"div">> = motion.div;
+const MotionHeader: React.FC<HTMLMotionProps<"header">> = motion.header;
+const MotionFooter: React.FC<HTMLMotionProps<"footer">> = motion.footer;
 
 export { MotionDiv, MotionHeader, MotionFooter };
